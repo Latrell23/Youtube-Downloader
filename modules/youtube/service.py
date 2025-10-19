f5b70926bd0517c2fa83c7cddac1e43682a5081c
Latrell23 <latrell.price23@gmail.com>
2025-10-19 05:09:16 -0500

    Change Queue from single worker to multiple
    workers of n amount now takes task from queue and execute them on a shared thread pool i was able to fulfil 500 requests with an average of 1 task in queue with a loop making a request every 1 sec

--- modules/youtube/service.py
+++ modules/youtube/service.py
@@ -2,7 +2,7 @@ from modules.youtube.templates import Parser, Searcher
 from concurrent.futures import ThreadPoolExecutor
 import asyncio
 from core.logging import AsyncLogger
-from core.settings import MAX_WORKERS, MAX_CYCLES
+from core.settings import MAX_WORKERS, MAX_CYCLES,MAX_THREADS
 from modules.youtube.parsers import VideoParser, ChannelParser
 from modules.youtube.search import QuickSearch, DetailedSearch
 from modules.youtube.models import SearchTask
@@ -16,7 +16,8 @@ class YouTubeService:
         self.logger = logger
         self.qs_queue = qs_queue
         self.running = True
-        self.thread_pool = ThreadPoolExecutor(MAX_WORKERS)
+        self.thread_pool = ThreadPoolExecutor(MAX_THREADS)
+        self.results = asyncio.Queue()
         
         if self.logger is None and self.debug is True:
                 print('[Error]: YouTubeService did not recieve a logger')
@@ -27,7 +28,7 @@ class YouTubeService:
         task = SearchTask(client_id=client_id, term=term)
         await self.qs_queue.enqueue(task)
             
-    async def start(self):
+    async def workers(self, on_result):
         ''' Continues to gather tasks from queue and executes them in their own thread '''
         loop = asyncio.get_running_loop()
         while self.running:
@@ -35,15 +36,24 @@ class YouTubeService:
             
             try:
                 executed_task:SearchTask = await loop.run_in_executor(self.thread_pool, self.quick_searcher.search, task)
+                await on_result(executed_task)
             except Exception as e:
                 print(e)
                 continue
             
-            yield executed_task
-            
             if executed_task.cycle != MAX_CYCLES:
                 #need to add function to check if client still active
                 asyncio.create_task(self.qs_queue.enqueue(executed_task))
+                
+    async def handle_results(self, task):
+        await asyncio.to_thread(print, f'Completed {task}')
+               
+    async def start_workers(self, num_workers:int = MAX_WORKERS):
+        workers = [
+            asyncio.create_task(self.workers(self.handle_results))
+            for i in range(num_workers)
+        ]
+        await asyncio.gather(*workers)
                 
 if __name__ == "__main__":
     import random
@@ -60,17 +70,19 @@ if __name__ == "__main__":
     
     async def print_task():
         search_task:SearchTask
-        async for search_task in youtube_service.start():
+        async for search_task in youtube_service.start_workers(num_workers=MAX_WORKERS):
             await aprint(search_task)
             
     async def queue_task():
         while True:
             id = random.randint(1,100)
-            await asyncio.sleep(random.randint(1,3))
+            await asyncio.sleep(1)
             await youtube_service.search(client_id='Tester', term=f'carti{id}')
             
     async def main():
-        await asyncio.gather(print_task(), queue_task())
+        await asyncio.gather(
+            youtube_service.start_workers(num_workers=5),
+            queue_task())
     
     try:
         asyncio.run(main())
